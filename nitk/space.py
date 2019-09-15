987bf426c5b052824ec16e0effa09d9f098b2364
Tristan Millington <tristan.millington@gmail.com>
2019-09-15 22:16:36 +0100

    Added comments and removed specific python solver, now is an option into the SPACE class

--- nitk/space.py
+++ nitk/space.py
@@ -18,6 +18,7 @@ class SPACE():
     See
     https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2770199/
     for more information
+
     Attributes
     ----------
     l1_reg : float 
@@ -33,17 +34,22 @@ class SPACE():
         precision matrix)
     weight_ : p by 1 array 
         Weight to give various nodes - currently not implemented
-    iter_ : 2
+    iter_ : int (default 2)
         Number of iterations to run the method for (2/3 is usually sufficient)
+    solver_: 'c' or 'python' (default 'c')
+        Whether to use the C code provided by the authors or a Python solver
+        for the problem (please note the Python solver can be very slow and use
+        a lot of memory)
     verbose : bool
         Whether to dump more information to the console
     """
-    def __init__(self, l1_reg, l2_reg=0, sig=None, weight=None, iter=2, verbose=False):
+    def __init__(self, l1_reg, l2_reg=0, sig=None, weight=None, iter=2, solver='c', verbose=False):
         self.sig_ = sig
         self.weight_ = weight
         self.partial_correlation_ = None
         self.precision_ = None
         self.iter_ = iter
+        self.solver_ = solver
         self.verbose_ = verbose
 
         # This is mostly magic so we can call the C code the authors have 
@@ -111,10 +117,16 @@ class SPACE():
         self.sig_ = np.ones(p, dtype=np.float32)
 
         for i in range(self.iter_):
-            self.partial_correlation_ = self.run_jsrm(X)
-            np.fill_diagonal(space_prec, 1)
+            if self.solver_ == 'c':
+                self.partial_correlation_ = self.run_jsrm(X)
+            elif self.solver_ == 'python':
+                lasso = lm.Lasso(self.l1_reg/(X_inp.shape[0]))
+                lasso.fit(X_inp, y_inp)
+                self.partial_correlation_[ind] = lasso.coef
+                self.partial_correlation_[ind] += self.partial_correlation_[ind]
+            np.fill_diagonal(self.partial_correlation_, 1)
             ind = np.triu_indices(p)
-            coef = space_prec[ind]
+            coef = self.partial_correlation_[ind]
             self.precision_ = self.create_precision_matrix_estimate(coef)
             self.sig_ = self.calculate_noise(X, beta)   
 
@@ -146,7 +158,7 @@ class SPACE():
         off diagonal estimates (coef)
         Parameters
         ----------
-        X : array_like
+        coef : array_like
             p by p matrix containing the estimates of the off-diagonal 
             values 
 
@@ -168,7 +180,18 @@ class SPACE():
 
     def build_input(self, X):
         """
-        Builds the inputs into the lasso regression problem. This is slow for anything non trivial!
+        Builds two large matrices new_X and new_Y that we can put into
+        a normal lasso solver to solve the SPACE problem. WARNING: These
+        could be very large for anything other than trivial problems
+
+        Parameters
+        ----------
+        X : array_like
+            n by p data matrix
+
+        Returns
+        -------
+        (new_X, new_y) - X and y matrices to input into a lasso solver
         """
         n, p = X.shape
         new_n = n * p
@@ -194,38 +217,32 @@ class SPACE():
                 x += 1
         new_y = X.flatten(order='F')
 
-        return new_X.tocoo(), new_y
-
-    def python_solve(self, X, iter=2):
-        """
-        Builds and solves the model in pure python. Slow for anything non-trivial!
-        """
-        n, p = X.shape
-        space_prec = np.zeros((p, p))
-        self.sig_ = np.ones(p, dtype=np.float32)
-        ind = np.triu_indices(p, k=1)
-        ind_2 = np.triu_indices(p)
-        #X = normalize(X, axis=1)
-
-        for i in range(iter):
-            X_inp, y_inp = self.build_input(X)
-            lasso = lm.Lasso(self.l1_reg/(X_inp.shape[0]))
-            lasso.fit(X_inp, y_inp)
-            coef = lasso.coef_
-            space_prec[ind] = coef
-            space_prec += space_prec.T
-            np.fill_diagonal(space_prec, 1)
-
-            coef = space_prec[ind_2]
-            beta = self.Beta_coef(coef, self.sig_)
-            self.sig_ = self.InvSig(X, beta)   
-            print(self.sig_)
-        return space_prec
-
+        return new_X, new_y
 
 class SPACE_BIC():
     """
-    Fits SPACE using BIC instead
+    Estimates a set of partial correlation matrices using the SPACE method
+    proposed by Peng et al and selects the one with the lowest BIC
+    
+    See
+    https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2770199/
+    for more information
+
+    Attributes
+    ----------
+    alpha_ : float 
+        Lasso regularization parameter that gives the lowest BIC
+    l2_reg : float
+        L2 regularization parameter
+    partial_correlation : p by p array
+        Estimated partial correlation matrix with the lowest BIC
+    sig_ : p by 1 array
+        Estimated noise in the problem (inverse of the diagonal of the 
+        precision matrix)
+    alphas_ : array_like
+        Array of l1 regularization values to run
+    verbose : bool
+        Whether to dump more information to the console
     """
     def __init__(self, verbose=False, l2_reg=0, alphas=None):
         self.verbose = verbose
@@ -235,11 +252,31 @@ class SPACE_BIC():
         self.alpha_ = None
         
         if alphas is None:
-            self.alphas_ = np.logspace(-3, 3, 50)
+            self.alphas_ = np.linspace(1, 200)
         else:
             self.alphas_ = alphas
 
     def _run(self, X, l1_reg, l2_reg=0):
+        """
+        Runs the SPACE problem and for the specified X and 
+        regularization parameters
+
+        Parameters
+        ----------
+        X : array_like
+            n by p data matrix
+
+        l1_reg : float
+            L1 regularization parameter
+
+        l2_reg : float
+            L2 regularization parameter
+
+        Returns
+        -------
+        (precision matrix, noise vector) - tuple containing a p by p precision
+        matrix estimate and a p by 1 noise vector 
+        """
         if self.verbose:
             print("Running %s" % l1_reg)
         s = SPACE(l1_reg, l2_reg)
@@ -247,18 +284,23 @@ class SPACE_BIC():
         return s.precision_, s.sig_
 
     def fit(self, X):
+        """
+        Fits the SPACE problem to X and finds the precision 
+        matrix with the lowest BIC
+
+        Parameters
+        ----------
+        X : array_like
+            n by p data matrix
+
+        Returns
+        -------
+        """
         n, p = X.shape
-        rv = norm()
-        #max_l = n**(3/2) * rv.cdf(1 - (0.1/(2*p**2)))  
-        #min_l = 0.01 * max_l
 
         rerun = True
 
-        min_l = 1
-        max_l = 200
-
         while rerun:
-            self.alphas_ = np.linspace(min_l, max_l)
             if self.verbose:
                 print("Lambda limits are from %s to %s" % (self.alphas_[0], self.alphas_[-1]))
 
@@ -283,6 +325,9 @@ class SPACE_BIC():
                 min_l = min_l*10
                 max_l = max_l*10
 
+            self.alphas_ = np.linspace(min_l, max_l)
+
+
         if self.verbose:
             print("Best lambda is at %s" % best_l)
             #print(bics)
@@ -291,6 +336,22 @@ class SPACE_BIC():
         self.outputs_ = outputs       
 
     def bic(self, X, prec, sig):
+        """
+        Calculates the BIC for the given SPACE solution
+
+        Parameters
+        ----------
+        X : array_like
+            n by p data matrix
+        prec : array_like
+            p by p estimate of the precision matrix
+        sig : array_like
+            p by 1 estimate of the noise
+
+        Returns
+        -------
+        float - BIC of the problem
+        """
         n, p = X.shape
         total_bic = 0
         indices = np.arange(p)
@@ -308,71 +369,6 @@ class SPACE_BIC():
             total_bic += n * np.log(residual) + np.log(n) * k
 
         return total_bic
-
-class SPACE_BIC_Python(SPACE_BIC):
-    """
-    BIC version that fits using Python implementation. This is slow, use the SPACE_BIC method for anything other than
-    trivial problems.
-    """
-    def __init__(self, verbose=False, l2_reg=0, alphas=None):
-        self.verbose = verbose
-        self.outputs_ = None
-        self.precision_ = None
-        self.l2_reg = l2_reg
-        self.alpha_ = None
-        self.alphas_ = None
-
-    def _run(self, X, l1_reg, l2_reg=0):
-        s = SPACE(l1_reg)
-        prec = s.python_solve(X, l1_reg)
-
-        return prec, s.sig_
-
-    def fit(self, X):
-        n, p = X.shape
-        #max_l = n**(3/2) * rv.cdf(1 - (0.1/(2*p**2)))  
-        #min_l = 0.01 * max_l
-        s = SPACE(1)
-        new_X, new_y = s.build_input(X)
-
-        if self.alphas_ is None:
-            max_l = np.abs(new_X.T @ new_y).max()
-            min_l = 0.0001 * max_l
-
-        rerun = True
-
-        while rerun:
-            self.alphas_ = np.logspace(np.log10(min_l), np.log10(max_l))
-            if self.verbose:
-                print("Lambda limits are from %s to %s" % (self.alphas_[0], self.alphas_[-1]))
-
-            outputs = Parallel(n_jobs=4)(delayed(self._run)(X, l, self.l2_reg) for l in self.alphas_)
-            bics = []
-            for prec, sig in outputs:
-                error = self.bic(X, prec, sig)
-                bics.append(error)
-            bics = np.array(bics)
-            min_err_i = np.argmin(bics)
-            best_l = self.alphas_[min_err_i]
-            rerun = False
-            if best_l == self.alphas_[0]:
-                rerun = True
-                print("WARNING: lambda is at the minimum value. It might be worth rerunning with a different set of alphas")
-                min_l = min_l*0.1
-                max_l = max_l*0.1
-            
-            if best_l == self.alphas_[-1]:
-                rerun = True
-                print("WARNING: lambda is at the maximum value. It might be worth rerunning with a different set of alphas")
-                min_l = min_l*10
-                max_l = max_l*10
-
-        if self.verbose:
-            print("Best lambda is at %s" % best_l)
-            #print(bics)
-        self.alpha_ = best_l
-        self.precision_ = outputs[min_err_i][0] 
-        self.outputs_ = outputs       
 
 if __name__=="__main__":
     # A trivial example to show the methods
