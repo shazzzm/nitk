9a2d588bcc330cec93631a53ca77bfbc5c910792
Tristan Millington <tristan.millington@gmail.com>
2019-09-15 21:35:08 +0100

    Turned scaled lasso into a more sklearn style estimator

--- nitk/scaled_lasso.py
+++ nitk/scaled_lasso.py
@@ -1,134 +1,125 @@
 import numpy as np
-import glmnet_python
-from glmnet import glmnet
-from sklearn.datasets import make_sparse_spd_matrix
 import methods
 import math
-
-import rpy2.robjects as robjects
-from rpy2.robjects.packages import importr
-import rpy2.robjects.numpy2ri
 import numpy as np
-import rpy2.rinterface as rinterface
 from sklearn.linear_model import lars_path, Lasso
+from sklearn.base import BaseEstimator
 
-rpy2.robjects.numpy2ri.activate()
-buf = []
-
-def f(x):
-    buf.append(x)
-#rinterface.set_writeconsole_regular(f)
-
-scalreg = importr('scalreg')
-
-def run(X, l2=0):
+class ScaledLasso(BaseEstimator):
     """
+    Solves a regression problem using the 'Scaled Lasso' - 
+    a lasso based solver that estimates its own regularization
+    parameter by estimating the noise in the problem
+    
+    See
+    https://arxiv.org/abs/1202.2723
+    http://www.jmlr.org/papers/volume14/sun13a/sun13a.pdf
+    https://www.jstor.org/stable/41720740?seq=1#page_scan_tab_contents
+
+    for more information
+
+    Attributes
+    ----------
+    coefs_ : p by 1 array
+        Estimated coefficients for the regression problem
+    best_alpha_ : float
+        Regression parameter estimated for the problem
+    noise_ : float
+        Estimated noise in the problem
     """
-    prec = scalreg.scalreg(X, lam="univ")
-    prec = np.array(prec[0])
-    return prec
+    def __init__(self):
+        self.coefs_ = None
+        self.best_alpha_ = None
+        self.noise_ = None
 
-def solve_scaled_lasso_r(X, y):
-    outp = scalreg.scalreg(X, y, lam="univ")
-    return (outp[0][0], outp[1], np.array(outp[2]))
+    def fit(self, X, y):
+        """
+        Solves a scaled lasso problem for X and y
+        Parameters
+        ----------
+        X : array_like
+            n by p matrix - data matrix
+        y : array_like
+            n by 1 matrix - produced values
 
-def calculate_noise(beta, cov):
-    p = cov.shape[0]
-    return beta.T @ cov @ beta
+        Returns
+        -------
+        """
+        n, p = X.shape
 
-def calculate_lambda(l_0, noise):
-    return noise * l_0
+        # Our initial estimate of lambda
+        lam0=np.sqrt(2*np.log(p)/n)
+        sigma = np.inf
+        new_sigma = 5
 
-def find_nearest_arg(array,value):
-    idx = np.searchsorted(array[::-1], value, side="left")
-    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
-        return idx-1
-    else:
-        return idx
+        for i in range(100):
+            sigma = new_sigma
+            lam = lam0 * sigma
+            lm = Lasso(alpha=lam)
+            lm.fit(X, y)
+            curr_coefs = lm.coef_
+            predicted_y = X @ curr_coefs 
+            new_sigma = np.sqrt(((y - predicted_y) ** 2).mean())
 
-def solve_scaled_lasso(X, y):
-    n, p = X.shape
-    alphas, active_coefficients, coefs = lars_path(X, y, method='lasso')
-    print(alphas)
-    lam0=np.sqrt(2*np.log(p)/n)
-    sigmaint=0.1
-    sigmanew=5
-    flag = 0
-    while abs(sigmaint-sigmanew)>0.0001 and flag <= 100:
-        flag += 1
-        sigmaint = sigmanew
-        lam = lam0 * sigmaint
-        print(lam)
+            if abs(new_sigma - sigma):
+                break
+
+        sigma = new_sigma
         lm = Lasso(alpha=lam)
         lm.fit(X, y)
-        #idx = find_nearest_arg(alphas, lam)
-        #curr_coefs = coefs[:, idx]
-        curr_coefs = lm.coef_
-        hy = X @ curr_coefs 
-        sigmanew = np.sqrt(((y - hy) ** 2).mean())
+        self.coefs_ = lm.coef_
+        self.noise_ = sigma
+        self.best_alpha_ = lam
 
-    hsigma = sigmanew
-    hlam = lam
-    lm = Lasso(alpha=lam)
-    lm.fit(X, y)
-    best_coef = lm.coef_
-    #best_alpha = alphas[idx]
-    #print(idx)
-    #best_coef = coefs[:, idx]
-    hy = X @ best_coef
-    return lam, best_coef, hsigma, y - hy, hy
+class ScaledLassoInference(BaseEstimator):
+    """
+    Estimates a precision matrix using the 'scaled lasso' - this
+    is a lasso based estimator that calculates its own regularization parameter
+    
+    See
+    https://arxiv.org/abs/1202.2723
+    http://www.jmlr.org/papers/volume14/sun13a/sun13a.pdf
+    https://www.jstor.org/stable/41720740?seq=1#page_scan_tab_contents
 
-def estimate_precision_matrix(X):
-    n, p = X.shape
-    indices = np.arange(p)
-    hsigma = np.zeros(p)
-    beta = np.zeros((p, p))
-    residuals = np.zeros((n, p))
-    for i in range(p):
-        scalefac = np.sqrt((X[:, indices!=i]**2).sum(axis=0)/n)
-        X_j = X[:, indices!=i]/scalefac
-        best_alpha, best_coef, hsigma_i, res, hy = solve_scaled_lasso(X_j, X[:, i])
-        #hsigma_i, best_coef, res = solve_scaled_lasso_r(X_j, X[:, i])
-        hsigma[i] = hsigma_i
-        beta[indices!=i, i] = best_coef/scalefac
-        residuals[:,i] = res
+    for more information.
 
-    hsigma = np.reciprocal(hsigma**2)
-    tTheta = np.diag(hsigma)
-    tTheta = -beta @ tTheta
-    hTheta = methods.make_matrix_symmetric(tTheta)
-    ind = np.diag_indices(p)
-    hTheta[ind] = hsigma
-    return tTheta
+    Attributes
+    ----------
+    precision_ : p by p matrix
+        Estimated precision matrix 
+    """
 
-def fit(X):
-    n, p = X.shape
-    l_0 = np.sqrt((2/n) * np.log(p))
-    cov = np.cov(X.T)
-    indices = np.arange(p)
-    precision_ = np.linalg.pinv(cov)
+    def __init__(self):
+        self.precision_ = None
 
-    for i in range(p):
-        for it in range(10):
-            noise = precision_[i, i]
-            coefs = solve_scaled_lasso(X[:, indices!=i], X[:, i], cov, noise, l_0).flatten()
-            beta = np.zeros(p)
-            beta[indices!=i] = coefs
-            beta[i] = -1
-            noise = calculate_noise(beta, cov)
-            prec_diag = np.diag(precision_)
-            precision_[i, indices!=i] = np.divide(coefs, prec_diag[indices!=i])
-            l_0 = calculate_lambda(l_0, noise)
-            precision_[i, i] = 1/noise
+    def fit(self, X):
+        """
+        Solves p scaled lassos problems to estimate the precision matrix
+        of X
+        Parameters
+        ----------
+        X : array_like
+            n by p matrix - data matrix
+        """
+        n, p = X.shape
+        indices = np.arange(p)
+        hsigma = np.zeros(p)
+        beta = np.zeros((p, p))
+        residuals = np.zeros((n, p))
+        for i in range(p):
+            scalefac = np.sqrt((X[:, indices!=i]**2).sum(axis=0)/n)
+            X_j = X[:, indices!=i]/scalefac
+            sl = ScaledLasso()
+            sl.fit(X_j, X[:, i])
+            
+            hsigma[i] = sl.noise_
+            beta[indices!=i, i] = sl.coefs_/scalefac
+            residuals[:,i] = np.power((X_i - X_j @ beta), 2)
 
-
-    print(precision_)
-
-p = 5
-n = 200
-K = make_sparse_spd_matrix(p, 0.7)
-C = np.linalg.inv(K)
-X = np.random.multivariate_normal(np.zeros(p), C, n)
-print(estimate_precision_matrix(X))
-print(run(X))
-print(K)
+        hsigma = np.reciprocal(hsigma**2)
+        tTheta = np.diag(hsigma)
+        tTheta = -beta @ tTheta
+        hTheta = methods.make_matrix_symmetric(tTheta)
+        ind = np.diag_indices(p)
+        hTheta[ind] = hsigma
+        self.precision_ = hTheta
