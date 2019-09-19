5e030abd82a6c4b8913a255bd96cf53d218b38e8
Tristan Millington <tristan.millington@gmail.com>
2019-09-19 12:01:24 +0100

    L2 penalised estimator now is sklearn-like and has a test

---
+++ nitk/l2_penalized_estimator.py
@@ -0,0 +1,131 @@
+"""
+Implements an L2 regularised covariance estimator - based on Scout by Witten and Tibshirani
+We solve -logdet Theta + tr(S Theta) + p ||Theta||_2^2
+"""
+import numpy as np
+import sklearn
+import scipy as sp
+import sklearn.datasets
+from sklearn.model_selection import KFold
+import collections
+from sklearn.utils.extmath import fast_logdet
+from sklearn.base import BaseEstimator
+
+class L2PenalizedEstimator(BaseEstimator):
+    """
+    Estimators an L2 penalized precision matrix
+
+    See:
+    https://www.sciencedirect.com/science/article/pii/S1532046413001019
+    Attributes
+    ----------
+    alpha_ : float
+        Regression parameter for the problem
+    precision_ : array_like
+        p by p array containing the estimated precision matrix
+    """
+
+    def __init__(self, alpha):
+        self.alpha_ = alpha
+        self.precision_ = None
+
+    def fit(self, X):
+        """
+        Solves the problem using eigendecomposition
+        
+        Parameters
+        ----------
+        X : array_like
+            n by p matrix - data matrix
+        Returns
+        -------
+        """
+        S = np.cov(X.T)
+        p = S.shape[0]
+        eigvals, eigvecs = np.linalg.eig(S)
+        new_eigvals = np.zeros(p)
+
+        for i in range(p):
+            s = eigvals[i]
+            e = -s/(4*self.alpha_) + np.sqrt(s**2 + 8*self.alpha_)/(4*self.alpha_)
+            new_eigvals[i] = e
+
+        D = np.diag(new_eigvals)
+        theta = eigvecs @ D @ eigvecs.T
+        self.precision_ = theta
+
+    def _precision_likelihood_function(self, S, theta):
+        """
+        Estimates the likelihood of the precision matrix theta
+        given the sample covariance matrix S
+        
+        Parameters
+        ----------
+        S : array_like
+            p by p matrix - sample covariance matrix
+        theta : array_like
+            p by p matrix - estimated precision matrix
+        Returns
+        -------
+        likelihood - float
+        Likelihood of the problem
+        """
+        p = S.shape[0]
+        log_likelihood_ = -fast_logdet(theta) + np.trace(S @ theta)    
+        log_likelihood_ -= p * np.log(2 * np.pi)
+        return log_likelihood_    
+
+class L2PenalizedEstimatorCV(L2PenalizedEstimator):
+    """
+    Estimators an L2 penalized precision matrix and uses 
+    cross-validation to estimate the regularization parameter
+    
+    Attributes
+    ----------
+    n_splits_ : int
+        Number of K-folds to use for the cross-validation
+    alpha_ : float
+        Regression parameter for the problem
+    precision_ : array_like
+        p by p array containing the estimated precision matrix
+    """
+    def __init__(self, n_splits=4):
+        self.n_splits_ = n_splits
+        self.alpha_ = None
+        self.precision_ = None
+
+    def fit(self, X):
+        """
+        Estimates an L2 penalized precision matrix using
+        cross-validation
+        
+        Parameters
+        ----------
+        X : array_like
+            n by p matrix - data matrix
+        Returns
+        -------
+        """
+        p = X.shape[1]
+        prec = np.zeros((p, p))
+        kf = KFold(n_splits = n_splits)
+        l_likelihood = collections.defaultdict(list)
+        for train, test in kf.split(X):
+            X_train = X[train, :]
+            X_test = X[test, :]
+            lambdas = np.logspace(-1, 1)
+            likelihoods = []
+
+            for l in lambdas:
+                est = L2PenalizedEstimator()
+                est.fit(X_train, l)
+                likelihood = self.precision_likelihood_function(S_test, est.precision_)
+                l_likelihood[l].append(likelihood)
+
+        likelihoods = []
+        for l in l_likelihood:
+            mean_likelihood = np.mean(l_likelihood[l])
+            likelihoods.append(mean_likelihood)
+        best_l_index = np.argmin(likelihoods)
+        self.precision_ = solve(X, lambdas[best_l_index])
+        self.alpha_ = lambdas[best_l_index]
---
+++ tests/test_l2_penalized_estimator.py
@@ -0,0 +1,26 @@
+import numpy as np
+from nitk import l2_penalized_estimator
+from sklearn.utils.testing import assert_array_almost_equal
+from sklearn.utils.testing import assert_array_less
+import unittest
+from sklearn.datasets import make_spd_matrix
+
+class TestL2Estimator(unittest.TestCase):
+
+    def test_l2_estimator(self):
+        """
+        Creates a distribution and ensures the estimator returns something
+        sensible
+        """
+        n = 100
+        p = 20
+        C = make_spd_matrix(p)
+        X = np.random.multivariate_normal(np.zeros(p), C, n)
+        est = l2_penalized_estimator.L2PenalizedEstimator(0.2)
+        est.fit(X)
+        # Check there is no NaNs
+        self.assertFalse(np.all(np.isnan(est.precision_)))
+
+        # Check the result is positive semi-definite
+        eigs, _ = np.linalg.eig(est.precision_)
+        self.assertTrue(np.all(eigs > 0))
